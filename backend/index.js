f752a63d811c5cac3467c438ec7be21d7467df99
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-20 20:01:17 +0200

    create the server and connect it to the DB

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,6 @@ const dotenv = require("dotenv").config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
 app.listen(3000, () => {
   connectDB();
   console.log("Server is running on port 3000");
