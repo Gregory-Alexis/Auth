0c4064101ad53014bb06720e25d8ad57a4c8b496
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-20 20:17:58 +0200

    create and import the auth route

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const connectDB = require("./db/connectDB");
+const authRoutes = require("./routes/auth");
 
 const dotenv = require("dotenv").config();
 
 const app = express();
+
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
