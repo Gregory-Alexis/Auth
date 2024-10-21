5caf48f13639e69da0a1a61b43d94a7395cb58cc
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-21 19:10:06 +0200

    rename the auth route, add an env variable for the port  and import express.json() to parse de data

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const connectDB = require("./db/connectDB");
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/auth_routes");
 
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use("/api/auth", authRoutes);
 
-app.listen(3000, () => {
+app.use(express.json());
+
+app.listen(5000, () => {
   connectDB();
-  console.log("Server is running on port 3000");
+  console.log("Server is running on port: ", PORT);
 });
