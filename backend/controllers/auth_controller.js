e4ff34ecc5f633070aac0f6f7aee25a55e5e645b
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-22 14:05:11 +0200

    create token and cookie function

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -1,12 +1,12 @@
 const User = require("../models/User_model");
 const bcrypt = require("bcrypt");
-const generateVerificationCode = require("../utils/generateVerificationCode");
+const generateTokenAndSetCookie = require("../utils/generateTokenAndSetCookie");
 
-export const signup = async (req, res) => {
+const signup = async (req, res) => {
   const { name, email, password } = req.body;
   try {
     if (!email || !password || !name) {
-      return res.status(400).json({ message: "All fields are required" });
+      return res.status(400).json({ success: false, message: "All fields are required" });
     }
 
     const userAlreadyExists = await User.findOne({ email });
@@ -15,21 +15,38 @@ export const signup = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const verificationCode = generateVerificationCode();
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
     const user = await User.create({
-      name,
       email,
       password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+
+    await user.save();
+
+    generateTokenAndSetCookie(res, user._id);
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      user: {
+        ...user._doc,
+        password: null,
+      },
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export const login = async (req, res) => {
+const login = async (req, res) => {
   res.send("Login route");
 };
 
-export const logout = async (req, res) => {
+const logout = async (req, res) => {
   res.send("Logout route");
 };
+
+module.exports = { signup, login, logout };
