5a5ca9698bbca2570e067ca9a5de25eb57a09967
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-22 14:03:54 +0200

    refactored due to bad position of express.json()

--- backend/index.js
+++ backend/index.js
@@ -7,9 +7,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth", authRoutes);
-
 app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => {
   connectDB();
