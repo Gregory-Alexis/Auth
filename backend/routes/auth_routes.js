4bd65a1da31ca87204309eb3d302e3f68feee78a
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-22 14:05:26 +0200

    change test get route to post

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -3,8 +3,8 @@ const { signup, login, logout } = require("../controllers/auth_controller");
 
 const router = express.Router();
 
-router.get("/signup", signup);
-router.get("/login", login);
-router.get("/logout", logout);
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
 
 module.exports = router;
