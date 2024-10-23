b449d70fd75184a7f18c726b5fc133e09b53d0ac
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-23 18:14:01 +0200

    function that send welcome email when a user create his account

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -12,7 +12,6 @@ const sendVerificationEmail = async (email, verificationToken) => {
       html: VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}", verificationToken),
       category: "Account Verification",
     });
-
     console.log("Email sent successfully", response);
   } catch (error) {
     console.error(`Error sending email: ${error.message}`);
@@ -20,4 +19,24 @@ const sendVerificationEmail = async (email, verificationToken) => {
   }
 };
 
-module.exports = sendVerificationEmail;
+const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "ab56b6ee-1504-4fa7-b8e3-c69ae582352b",
+      template_variables: {
+        company_info_name: "Auth Company",
+        name: name,
+      },
+    });
+    console.log("Welcome email sent successfully", response);
+  } catch (error) {
+    console.error(`Error sending email: ${error.message}`);
+    throw new Error(`Error sending email: ${error.message}`);
+  }
+};
+
+module.exports = { sendVerificationEmail, sendWelcomeEmail };
