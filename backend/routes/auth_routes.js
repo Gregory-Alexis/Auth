333879a6a5bf012766f4168038bb79e06a5206e8
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-23 21:21:05 +0200

    add forgotPassword, resetPassword, checkAuth routes

--- backend/routes/auth_routes.js
+++ backend/routes/auth_routes.js
@@ -1,12 +1,26 @@
 const express = require("express");
-const { signup, login, logout, verifyEmail } = require("../controllers/auth_controller");
+const verifyToken = require("../middleware/verifyToken");
+const {
+  signup,
+  login,
+  logout,
+  verifyEmail,
+  forgotPassword,
+  resetPassword,
+  checkAuth,
+} = require("../controllers/auth_controller");
 
 const router = express.Router();
+
+router.get("/check-auth", verifyToken, checkAuth);
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 
 router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
+
+router.post("/reset-password/:token", resetPassword);
 
 module.exports = router;
