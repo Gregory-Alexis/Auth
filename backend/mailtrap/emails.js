923fbb56434a9fd280add85289a75c648f6cb43a
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-23 21:14:33 +0200

    create sendPasswordResetEmail when the user reset his password and sendResetSuccessEmail to specify to the user that is password has been successfuly changed

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,4 +1,8 @@
-const { VERIFICATION_EMAIL_TEMPLATE } = require("./emailTemplate");
+const {
+  VERIFICATION_EMAIL_TEMPLATE,
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
+} = require("./emailTemplate");
 const { mailtrapClient, sender } = require("./mailtrap.config");
 
 const sendVerificationEmail = async (email, verificationToken) => {
@@ -39,4 +43,46 @@ const sendWelcomeEmail = async (email, name) => {
   }
 };
 
-module.exports = { sendVerificationEmail, sendWelcomeEmail };
+const SendPasswordResetEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Password Reset",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+    });
+
+    console.log("Password reset email sent successfully", response);
+  } catch (error) {
+    console.log("Error sending password reset email", error);
+    throw new Error(`Error sending password reset email: ${error.message}`);
+  }
+};
+
+const sendResetSuccessEmail = async (email) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Password Reset Success",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    });
+
+    console.log("Password reset success email sent successfully", response);
+  } catch (error) {
+    console.log("Error sending password reset success email", error);
+    throw new Error(`Error sending password reset success email: ${error.message}`);
+  }
+};
+
+module.exports = {
+  sendVerificationEmail,
+  sendWelcomeEmail,
+  SendPasswordResetEmail,
+  sendResetSuccessEmail,
+};
