ffb037f7f419c22c99566054151cbd20ac5e3dc0
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-26 21:47:18 +0200

    add the first 3 routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import SingUp from "./pages/SingUp";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-red-500 text-5xl">Hello world</h1>
-    </>
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+      <Routes>
+        <Route path="/" element={"Home"} />
+        <Route path="/signup" element={<SingUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
