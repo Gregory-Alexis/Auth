9efec4c34348e5f1097766e9e7cb10c89acdde18
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-27 21:26:41 +0100

    import cors

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,23 @@
-const express = require("express");
-const connectDB = require("./db/connectDB");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const connectDB = require('./db/connectDB');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
-const authRoutes = require("./routes/auth_routes");
+const authRoutes = require('./routes/auth_routes');
 
-require("dotenv").config();
+require('dotenv').config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(5000, () => {
   connectDB();
-  console.log("Server is running on port: ", PORT);
+  console.log('Server is running on port: ', PORT);
 });
