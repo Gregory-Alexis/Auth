dfac548fc1974bb377d782ec3aa72d96722a3c15
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-29 21:58:28 +0100

    use store for signing up and redirect to the /verifify-email endpoint if the account is successfuly created

--- frontend/src/pages/SingUp.jsx
+++ frontend/src/pages/SingUp.jsx
@@ -1,18 +1,28 @@
-import { motion } from "framer-motion";
-import { User, Mail, Lock } from "lucide-react";
-import { useState } from "react";
+import { motion } from 'framer-motion';
+import { User, Mail, Lock, Loader } from 'lucide-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-import Input from "../components/Input";
-import { Link } from "react-router-dom";
-import PasswordStrengthMeter from "../components/PasswordStrengthMeter";
+import Input from '../components/Input';
+import PasswordStrengthMeter from '../components/PasswordStrengthMeter';
+import { useAuthStore } from '../store/authStore';
 
 const SingUp = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const { signUp, isLoading, error } = useAuthStore();
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
+
+    try {
+      await signUp(name, email, password);
+      navigate('/verify-email');
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -20,57 +30,60 @@ const SingUp = () => {
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
-      className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl overflow-hidden"
+      className='max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl overflow-hidden'
     >
-      <div className="p-8">
-        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+      <div className='p-8'>
+        <h2 className='text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text'>
           Create Account
         </h2>
 
         <form onSubmit={handleSignUp}>
           <Input
             icon={User}
-            type="text"
-            placeholder="Full Name"
+            type='text'
+            placeholder='Full Name'
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
 
           <Input
             icon={Mail}
-            type="email"
-            placeholder="Email Adress"
+            type='email'
+            placeholder='Email Adress'
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
 
           <Input
             icon={Lock}
-            type="password"
-            placeholder="Password"
+            type='password'
+            placeholder='Password'
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
 
+          {error && <p className='text-red-500 font-semibold mt-2'>{error}</p>}
+
           <PasswordStrengthMeter password={password} />
 
           <motion.button
-            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+            className='mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
 						font-bold rounded-lg shadow-lg hover:from-green-600
 						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
-						 focus:ring-offset-gray-900 transition duration-200"
+						 focus:ring-offset-gray-900 transition duration-200'
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
-            type="submit"
+            type='submit'
+            disabled={isLoading}
           >
-            Sign Up
+            {isLoading ? <Loader className=' animate-spin mx-auto' size={24} /> : 'Sign Up'}
           </motion.button>
         </form>
       </div>
-      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
-        <p className="text-sm text-gray-400">
-          ALready have an account ?{" "}
-          <Link to={"/login"} className="text-green-400 hover:underline">
+      <div className='px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center'>
+        <p className='text-sm text-gray-400'>
+          ALready have an account ?{' '}
+          <Link to={'/login'} className='text-green-400 hover:underline'>
             Login
           </Link>
         </p>
