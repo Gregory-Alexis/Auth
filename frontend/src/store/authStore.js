879a1c2f1f7f8cbd609e6d33a38e02e91df2889b
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-31 17:09:27 +0100

    create store to login, logout and check if user is authenticated

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -30,20 +30,59 @@ export const useAuthStore = create((set) => ({
       throw error;
     }
   },
+  login: async (email, password) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/login`, { email, password });
+      set({
+        isAuthenticated: true,
+        user: response.data.user,
+        error: null,
+        isLoading: false,
+      });
+    } catch (error) {
+      console.log('Error', error);
+      set({
+        error: error.response.data.message || 'Error logging in',
+        isLoading: false,
+      });
+      throw new Error(error.response.data.message);
+    }
+  },
+
+  logout: async () => {
+    set({ isLoading: true, error: null });
+    try {
+      await axios.post(`${API_URL}/logout`);
+      set({ user: null, isAuthenticated: false, error: null, isLoading: false });
+    } catch (error) {
+      set({ error: 'Error logging out', isLoading: false });
+      throw error;
+    }
+  },
 
   verifyEmail: async (code) => {
     set({ isLoading: true, error: null });
     try {
       const response = await axios.post(`${API_URL}/verify-email`, { code });
-      set({ user: response.data.user, isAuthenticated: true, isLoading: false, error: true });
+      set({ isAuthenticated: true, user: response.data.user, isLoading: false, error: true });
       return response.data;
     } catch (error) {
       set({
         error: error.response.data.message || 'Error verifying email',
         isLoading: false,
-        error: true,
       });
       throw error;
+    }
+  },
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
+    try {
+      const response = await axios.get(`${API_URL}/check-auth`);
+      set({ user: response.data.user, isAuthenticated: true, isCheckingAuth: false });
+    } catch (error) {
+      set({ error: null, isCheckingAuth: false, isAuthenticated: false });
     }
   },
 }));
