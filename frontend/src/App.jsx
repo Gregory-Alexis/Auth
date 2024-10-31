6cd2656f85b612c85f128b534d5ef7c98a80e116
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-10-31 15:58:06 +0100

    add verify email route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
 
-import Login from "./pages/Login";
-import SingUp from "./pages/SingUp";
+import Login from './pages/Login';
+import SingUp from './pages/SingUp';
+import EmailVerification from './pages/EmailVerification';
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden">
+    <div className='min-h-screen bg-gradient-to-br from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
       <Routes>
-        <Route path="/" element={"Home"} />
-        <Route path="/signup" element={<SingUp />} />
-        <Route path="/login" element={<Login />} />
+        <Route path='/' element={'Home'} />
+        <Route path='/signup' element={<SingUp />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/verify-email' element={<EmailVerification />} />
       </Routes>
+      <Toaster />
     </div>
   );
 }
