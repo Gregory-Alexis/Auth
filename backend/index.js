1247b2b50265c4a2ac013f98cf19a7a7a9e38b26
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-02 17:13:05 +0100

    add config for production to run the app on the port 5000

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const connectDB = require('./db/connectDB');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 const authRoutes = require('./routes/auth_routes');
 
 require('dotenv').config();
 
 const app = express();
-
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
@@ -17,7 +17,15 @@ app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
-app.listen(5000, () => {
-  connectDB();
-  console.log('Server is running on port: ', PORT);
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+  });
+
+  app.listen(5000, () => {
+    connectDB();
+    console.log('Server is running on port: ', PORT);
+  });
+}
